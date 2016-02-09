0551e6b1e13252437a02884f1f162519674e8bc7
Ashutosh Singh <techashu@outlook.com>
2016-02-09 18:23:17 +0530

    Views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ("Hello world from my Blog")
+    data = {'mydata' : "Hello World"}
+    return render(request, 'blog/index.html', data)
